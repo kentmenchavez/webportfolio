7b2f0ed94cb2b4292f16ae67e419cbd691701e98
kentmenchavez89 <kentmenchavez89@gmail.com>
2024-03-03 17:19:38 +0800

    added updates

---
+++ index.js
@@ -0,0 +1,9 @@
+function collapseNavbar() {
+  var navbarCollapse = document.querySelector('.navbar-collapse');
+  if (navbarCollapse.classList.contains('show')) {
+    navbarCollapse.classList.remove('show');
+  }
+}
+
+
+
